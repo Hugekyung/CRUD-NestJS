d3f23a27f4d630f57041f7602df0127f21aa1e0b
haechan <kiki9510@gmail.com>
2022-04-18 00:02:49 +0900

    Added hash password system with argon2 lib

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signup() {
-    return { message: 'Sign Up !' }; // json 형식으로 응답한다
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+    });
+    delete user.hash; // user객체에서 비밀번호 정보 삭제
+
+    return user; // json 형식으로 응답한다
   }
 
   signin() {
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true })); // dto에 지정한 데이터만 걸러서 가져온다
   await app.listen(3000);
 }
 bootstrap();
