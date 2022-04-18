3517fc57283ee5ed70e7ca24106487be0cb48e92
haechan <kiki9510@gmail.com>
2022-04-19 00:23:49 +0900

    Added Error Handler in signup logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
@@ -8,15 +8,23 @@ export class AuthService {
   constructor(private prisma: PrismaService) {}
   async signup(dto: AuthDto) {
     const hash = await argon.hash(dto.password);
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-    });
-    delete user.hash; // user객체에서 비밀번호 정보 삭제
 
-    return user; // json 형식으로 응답한다
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      delete user.hash; // user객체에서 비밀번호 정보 삭제
+      return user; // json 형식으로 응답한다
+    } catch (error) {
+      console.log(error);
+      if (error.code === 'P2002') {
+        throw new ForbiddenException('This Email is already exist.');
+      }
+      throw error;
+    }
   }
 
   signin() {
