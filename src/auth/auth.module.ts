e347a4d2797e1d64df09bc3909929b7ffa36eb2d
haechan <kiki9510@gmail.com>
2022-04-20 01:14:01 +0900

    Added jwt init with passport

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
+  imports: [JwtModule.register({})],
   controllers: [AuthController],
   providers: [AuthService],
 })
