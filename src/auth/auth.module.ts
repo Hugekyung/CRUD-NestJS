69002deaa99f094f13a6f78065b8587db7c09e92
haechan <kiki9510@gmail.com>
2022-04-21 11:58:24 +0900

    Added signToken in auth.service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,10 +3,15 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService, private jwt: JwtService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
   async signup(dto: AuthDto) {
     const hash = await argon.hash(dto.password);
@@ -45,7 +50,19 @@ export class AuthService {
       throw new ForbiddenException('Doesn`t matched password');
     }
 
-    delete user.hash;
-    return user; // json 형식으로 응답한다
+    return this.signToken(user.id, user.email); // json 형식으로 응답한다
+  }
+
+  signToken(userId: number, email: string): Promise<string> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+    const secret = this.config.get('JWT_SECRET');
+
+    return this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: secret,
+    });
   }
 }
